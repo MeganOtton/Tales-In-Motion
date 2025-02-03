f5b11a541e73f536fd2d15fb9347b811ca9c2caf
Megan Otton <115323221+MeganOtton@users.noreply.github.com>
2025-02-03 14:17:15 +0000

    JS, Start Chapter Progression
    EDIT, Images Replaced Higher Res,
    ADD, Bedroom Door and Window,
    EDIT, Name input not disappearing. MUST FIX

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,27 +5,28 @@ let playerName = "";
 const chapters = {
     start: {
         title: "Chapter 1: Introduction",
-        image: "assets/images/open-door.png",
+        image: "assets/images/front-door.png",
         description: "Hello Adventurer! Before we get started on our journey, we’re going to need a name! So, What is your name, Adventurer?",
     },
     chapter1: {
         title: "Chapter 2: The Journey Begins",
-        image: "assets/images/open-door.png",
+        image: "assets/images/front-door.png",
         description: "After a lovely walk in the forest with your family, you've returned home to your small cottage, the front door which you’re sure you closed ajar. {playerName}, What do you do?",
         options: {
-            a: "Explore Inside",
-            b: "Explore Outside"
+            a: "chapter2",
+            b: "chapter1A"
         }
     },
-    chapter2: {
-        title: "Chapter 2",
-        image: "assets/images/chapter2.png",
-        description: "Description of chapter 2",
+    chapter1A: {
+        title: "Chapter 2: Explore Outside",
+        image: "assets/images/bedroom-window.png",
+        description: "You’ve decided to walk around the cottage. As you walk around the cottage you notice sparkly shards on the grass floor, the bedroom window shattered. Sharp rectangular shards stick up around the edges of the frame, too perilous for someone of your size to climb through.",
         options: {
-            a: "chapter5",
-            b: "chapter6"
+            a: "chapter1",
         }
     },
+
+
     // Add more chapters as needed
 };
 
@@ -68,9 +69,11 @@ function updateCard(chapter) {
     if (chapter === "start") {
         optionA.style.display = "none";
         optionB.style.display = "none";
+        document.querySelector(".input-area").style.display = "flex";
     } else {
         optionA.style.display = "block";
         optionB.style.display = "block";
+        document.querySelector(".input-area").style.display = "none";
         optionA.onclick = () => updateCard(chapters[chapter].options.a);
         optionB.onclick = () => updateCard(chapters[chapter].options.b);
 
